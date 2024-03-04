340e2251473055a4cef141096ed78751558a16b5
Anushree K N <151710243+anu8222@users.noreply.github.com>
2024-03-04 12:31:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function order() {
+alert('Thank You! Your Order has been successfully submitted Thanks!');
+}
+
+function submit() {
+alert('Thank You! Your details has been successfully submitted Thanks!');
+}
+
+function openForm(){
+document.getElementById("myForm").style.display="block";}
+
+function closeForm() {
+document.getElementById("myForm").style.display="none";
+}
